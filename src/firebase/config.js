2c06f4a7a89339d41dd860059ce181ab98fcb2ea
ilosrim <ilosrim@yahoo.com>
2022-01-13 22:13:58 +0500

    portfolio website

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -12,12 +12,12 @@ import '@firebase/firestore'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  authDomain: "ilosrim.firebaseapp.com",
+  projectId: "ilosrim",
+  storageBucket: "ilosrim.appspot.com",
+  messagingSenderId: "18566296721",
+  appId: "1:18566296721:web:fcd41a9077829596c07b30",
+  measurementId: "G-CCQ1L4X51G"
 };
 
 // Initialize Firebase
