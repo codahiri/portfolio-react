8928f18a31e96af957d6a4242c057b817bdcf1b7
ilosrim <ilosrim@yahoo.com>
2022-01-27 21:47:49 +0500

    visitors section add

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -7,6 +7,10 @@ function Footer(props) {
       <p className='footerTitle' >
         <a className='footerLogo' href='https://t.me/ilosrim' target='_blank' rel="noreferrer" >&copy; ilosrim </a> 2021 - {new Date().getFullYear()} | Made with <FavoriteIcon className='heartIcon' />
       </p>
+      <div className='visits'>
+        <i class="fa fa-eye" aria-hidden="true"></i>
+        <span id="visits"></span>
+      </div>
     </div>
   );
 }
